1bbcc362da109a115978b80e3332d25556cf0169
lstevensN <lstevens@student.neumont.edu>
2023-11-13 10:59:06 -0700

    world 7

---
+++ Source/Application/World07.cpp
@@ -0,0 +1,112 @@
+#include "World07.h"
+#include "Framework/Framework.h"
+#include "Input/InputSystem.h"
+
+#include <glm/glm/gtc/type_ptr.hpp>
+#include <glm/glm/gtx/color_space.hpp>
+
+#define INTERLEAVE
+
+namespace nc
+{
+    bool World07::Initialize()
+    {
+        m_scene = std::make_unique<Scene>();
+        m_scene->Load("scenes/scene_framebuffer.json");
+        m_scene->Initialize();
+
+        auto texture = std::make_shared<Texture>();
+        texture->CreateTexture(256, 256);
+        ADD_RESOURCE("fb_texture", texture);
+
+        auto framebuffer = std::make_shared<Framebuffer>();
+        framebuffer->CreateFramebuffer(texture);
+        ADD_RESOURCE("fb", framebuffer);
+
+        auto material = GET_RESOURCE(Material, "materials/postprocess.mtrl");
+        if (material)
+        {
+            material->albedoTexture = texture;
+        }
+
+        return true;
+    }
+
+    void World07::Shutdown()
+    {
+
+    }
+
+    void World07::Update(float dt)
+    {
+        m_time += dt;
+
+        ENGINE.GetSystem<Gui>()->BeginFrame();
+
+        m_scene->Update(dt);
+        m_scene->ProcessGui();
+
+        // set post-process GUI
+        ImGui::Begin("Post-Process");
+        ImGui::SliderFloat("Blend", &m_blend, 0, 1);
+
+        bool effect = m_params & INVERT_MASK;
+        if (ImGui::Checkbox("Invert", &effect))
+        {
+            (effect) ? m_params |= INVERT_MASK : m_params &= ~INVERT_MASK;
+        }
+        
+        effect = m_params & GRAYSCALE_MASK;
+        if (ImGui::Checkbox("Grayscale", &effect))
+        {
+            (effect) ? m_params |= GRAYSCALE_MASK : m_params &= ~GRAYSCALE_MASK;
+        }
+        
+        effect = m_params & COLOR_TINT_MASK;
+        if (ImGui::Checkbox("Color Tint", &effect))
+        {
+            (effect) ? m_params |= COLOR_TINT_MASK : m_params &= ~COLOR_TINT_MASK;
+        }
+
+        ImGui::End();
+
+        // set post-process shader
+        auto program = GET_RESOURCE(Program, "shaders/postprocess.prog");
+        if (program)
+        {
+            program->Use();
+            program->SetUniform("blend", m_blend);
+            program->SetUniform("params", m_params);
+        }
+
+        ENGINE.GetSystem<Gui>()->EndFrame();
+    }
+
+    void World07::Draw(Renderer& renderer)
+    {
+        // *** PASS 1 *** //
+        m_scene->GetActorByName("postprocess")->active = false;
+
+        auto framebuffer = GET_RESOURCE(Framebuffer, "fb");
+        renderer.SetViewport(framebuffer->GetSize().x, framebuffer->GetSize().y);
+        framebuffer->Bind();
+
+        renderer.BeginFrame({ 0, 0, 1 });
+        m_scene->Draw(renderer);
+
+        framebuffer->Unbind();
+
+
+        // *** PASS 2 *** //
+        m_scene->GetActorByName("postprocess")->active = true;
+
+        renderer.ResetViewport();
+        renderer.BeginFrame();
+        m_scene->GetActorByName("postprocess")->Draw(renderer);
+
+
+        // post-render
+        ENGINE.GetSystem<Gui>()->Draw();
+        renderer.EndFrame();
+    }
+}
